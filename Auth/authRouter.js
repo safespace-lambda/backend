6b800c804cb3dec0a306610dc4834670e462cf44
Gill Abada <gill@callifi.com>
2019-04-15 17:17:24 -0400

    revert pgsql.

--- Auth/authRouter.js
+++ Auth/authRouter.js
@@ -39,15 +39,12 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: 'Submit both username and password when logging in!' });
+  }
   try {
-    if (!username || !password) {
-      return res
-        .status(400)
-        .json({
-          message: 'Submit both username and password when logging in!'
-        });
-    }
     const user = await db('users')
       .where({ username })
       .first();
