5217a20fdee36d3fd62dfbd8ed46b6eceb904401
Gill Abada <gill@callifi.com>
2019-04-15 17:04:53 -0400

    test PK user table.

--- data/migrations/20190414135615_users.js
+++ data/migrations/20190414135615_users.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', tbl => {
-    tbl.increments();
+    tbl.increments('id');
     tbl
       .string('username')
       .notNullable()
