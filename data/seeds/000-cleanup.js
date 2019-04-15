a3d03ff7072b06574eb09429fcde82754f8af587
UnknownMonk <smodcartoon@gmail.com>
2019-04-15 15:26:33 -0400

    added clean up

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,15 +1,9 @@
-
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-
+const cleaner = require('knex-cleaner');
+// will prevent foreign key errors
+// when trying to truncate the tables that have
+// FK pointing to them
+exports.seed = function(knex) {
+  return cleaner.clean(knex);
+  // after this command, all tables are empty
+  // and all primary keys are reset
 };
