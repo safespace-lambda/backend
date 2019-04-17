176763d6b5ba790ed30618c59404f4a0a50b6a02
UnknownMonk <smodcartoon@gmail.com>
2019-04-17 08:12:33 -0400

    added middleware to auth routes restricted

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
+server.use('/api/auth', restricted, authRouter);
 server.use('/api/messages', messagesRouter);
 
 //sanity check
