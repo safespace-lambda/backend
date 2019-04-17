82457d276cb8d8ddbf68f2393c68732d6cc920db
UnknownMonk <smodcartoon@gmail.com>
2019-04-17 08:52:16 -0400

    Routes are restricte using middleware

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 
 const authRouter = require('../Auth/authRouter');
 const messagesRouter = require('../Messages/messagesRouter');
+const profilesRouter = require('../Profiles/profileRouter');
 
 const restricted = require('../middleware/restricted');
 const server = express();
@@ -13,8 +14,9 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', restricted, authRouter);
-server.use('/api/messages', messagesRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/messages', restricted, messagesRouter);
+server.use('/api/profiles', restricted, profilesRouter);
 
 //sanity check
 server.get('/', (req, res) => {
