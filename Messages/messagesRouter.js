e483b811864f5b29c7e98df81c6df88eec82ebdf
UnknownMonk <smodcartoon@gmail.com>
2019-04-17 13:52:21 -0400

    putroute message

--- Messages/messagesRouter.js
+++ Messages/messagesRouter.js
@@ -25,12 +25,13 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { body, user_id } = req.body;
-  if (!user_id || !body) {
+  const { body } = req.body;
+  const user_id = req.decodedToken.subject;
+  if (!body) {
     return res.status(422).json({ error: 'Missing required data' });
   } else {
     try {
-      const message = await Messages.add(req.body);
+      const message = await Messages.add({ ...req.body, user_id });
       res.status(201).json(message);
     } catch (error) {
       res.status(500).json(error);
@@ -52,22 +53,27 @@ router.get('/:id', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  try {
-    const count = await db('profile')
-      .where({ id: req.params.id })
-      .update(req.body);
-
-    if (count > 0) {
-      const profile = await db('profile')
+  const currentUserId = req.decodedToken.subject;
+  if (req.headers.id != currentUserId) {
+    res.status(401).json({ error: 'Stop trying to snoop!' });
+  } else {
+    try {
+      const count = await db('profile')
         .where({ id: req.params.id })
-        .first();
+        .update(req.body);
 
-      res.status(200).json(profile);
-    } else {
-      res.status(404).json({ message: 'Profile not found' });
+      if (count > 0) {
+        const profile = await db('profile')
+          .where({ id: req.params.id })
+          .first();
+
+        res.status(200).json(profile);
+      } else {
+        res.status(404).json({ message: 'Profile not found' });
+      }
+    } catch (error) {
+      res.status(500).json(error);
     }
-  } catch (error) {
-    res.status(500).json(error);
   }
 });
 
