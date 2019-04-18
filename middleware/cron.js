57e0b89f7087b0f497f680f9be02fef6f7ffbc4e
Jonas A Walden <smodcartoon@gmail.com>
2019-04-18 10:31:28 -0400

    Revert "add twilio SMS integration."

--- middleware/cron.js
+++
@@ -1,7 +0,0 @@
-const CronJob = require('../lib/cron.js').CronJob;
-const { findAndSend } = require('../sms/sms.js');
-
-console.log('Before job instantiation');
-const job = new CronJob('2 * * * * *', findAndSend());
-console.log('After job instantiation');
-job.start();
