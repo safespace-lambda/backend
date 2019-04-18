fed6ef7b2388590317560e3db043d24852815e88
Gill Abada <gill@callifi.com>
2019-04-18 10:07:13 -0400

    add twilio SMS integration.

--- sms/sms.js
+++ sms/sms.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_TEST_AUTH;
+const authToken = process.env.TWILIO_AUTH;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+15038556132',
-    to: '+12125551212'
-  })
-  .then(message => console.log(message.sid));
+const findAndSend = async () => {
+  const mapedData = await sms.find();
+
+  mapedData.map(data => {
+    client.messages
+      .create({
+        body: data.body,
+        from: '+15038556132',
+        to: process.env.TEST_CELL
+      })
+      .then(message => console.log(message.sid));
+  });
+};
+
+module.exports = {
+  findAndSend
+};
